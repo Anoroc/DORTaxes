fe35a27ba83cdefae8d4bf8f10faea1a737c47b1
Chad Youngblut <mptheory@gmail.com>
2020-12-11 13:00:20 -0500

    Provide a better description of the funciton

--- taxes.js
+++ taxes.js
@@ -1,4 +1,4 @@
-//Simple funciton to accept gross sales and totaltaxes collected. The function outputs total amount of sales taxes.
+//Simple funciton to accept gross sales and totaltaxes collected. The function outputs total amount of sales with taxes and sales that exlucded taxes for given period.
 
 const taxValue = 0.07 // The percentage charged for taxable sales
 
