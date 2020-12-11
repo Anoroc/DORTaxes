80cd5f3143a1e2087b01e0f18d7523c46e17f8c7
Chad Youngblut <mptheory@gmail.com>
2020-12-11 12:42:01 -0500

    Initial commit

---
+++ taxes.js
@@ -0,0 +1,16 @@
+//Simple funciton to accept gross sales and totaltaxes collected. The function outputs total amount of sales taxes.
+
+const taxValue = 0.07 // The percentage charged for taxable sales
+
+const taxDue = (grossSales, taxesCollected) => {
+  let salesWithTax = taxesCollected / taxValue
+
+  return `-------------- Tax Report -----------------
+            Taxes Collected: $${taxesCollected}
+                Gross Sales: $${grossSales}
+        Sales Including Tax: $${salesWithTax}
+        Sales Excluding Tax: $${grossSales - salesWithTax}
+  `
+}
+
+console.log(taxDue(7236.71, 45.99))
