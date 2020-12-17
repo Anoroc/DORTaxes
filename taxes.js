4e1948671d2c2bd7000e0639f38f4a44997f84b8
Chad Youngblut <mptheory@gmail.com>
2020-12-17 15:49:53 -0500

    Add discretionary tax and better number formatting

--- taxes.js
+++ taxes.js
@@ -1,16 +1,19 @@
-//Simple funciton to accept gross sales and totaltaxes collected. The function outputs total amount of sales with taxes and sales that exlucded taxes for given period.
+//Simple funciton that accept gross sales and totaltaxes collected. The function outputs total amount of sales with taxes, sales that exlucded taxes and descretionary surtax for a given period.
 
 const taxValue = 0.07 // The percentage charged for taxable sales
+const surtax = 0.01 // The designated percentage for your given county
 
 const taxDue = (grossSales, taxesCollected) => {
-  let salesWithTax = taxesCollected / taxValue
+  let salesWithTax = (taxesCollected / taxValue).toFixed(2)
+  let surtaxTotal = (salesWithTax * surtax).toFixed(2)
 
   return `-------------- Tax Report -----------------
-            Taxes Collected: $${taxesCollected}
-                Gross Sales: $${grossSales}
-        Sales Including Tax: $${salesWithTax}
-        Sales Excluding Tax: $${grossSales - salesWithTax}
+             Taxes Collected: $${taxesCollected}
+                 Gross Sales: $${grossSales}
+         Sales Including Tax: $${salesWithTax}
+         Sales Excluding Tax: $${grossSales - salesWithTax}
+        Discretionary Surtax: $${surtaxTotal}
   `
 }
 
-console.log(taxDue(7236.71, 45.99))
+console.log(taxDue(10000.0, 110.0))
