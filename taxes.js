1e18c636be3a5abaed5d096eeb8577180db33266
Anoroc <44936616+Anoroc@users.noreply.github.com>
2020-12-21 06:40:57 -0500

    Update taxes.js
    Update comment

--- taxes.js
+++ taxes.js
@@ -1,4 +1,4 @@
-//Simple funciton that accept gross sales and totaltaxes collected. The function outputs total amount of sales with taxes, sales that exlucded taxes and descretionary surtax for a given period.
+//Simple funciton that accepts gross sales and totaltaxes collected. The function outputs total amount of sales with taxes, sales that exlucded taxes and descretionary surtax for a given period.
 
 const taxValue = 0.07 // The percentage charged for taxable sales
 const surtax = 0.01 // The designated percentage for your given county
